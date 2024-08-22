bf005295113cd09328d55e0c7dae483bf1c6651a
kar-98-w <karlmarron25@gmail.com>
2024-08-22 13:23:33 +0800

    first commit in laptop

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[I am Karl , 20 years old and an IT student here at the University of the Assumption.]</p>
+        <p>
+          (This is just a practice on using Javascript on our 3rd year in IT Course here at the University of the Assumption.{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
